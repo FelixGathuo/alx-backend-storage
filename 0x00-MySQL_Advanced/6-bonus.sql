06cc14c32a17709b7beaa1065ec3347b4ca56bef
FelixGathuo <40053042+FelixGathuo@users.noreply.github.com>
2023-01-11 20:42:15 +0300

    Update 6-bonus.sql

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -1,7 +1,7 @@
 -- script that creates a stored procedure 
 -- AddBonus that adds a new correction for a student.
 
-CREATE PROCEDURE AddBonus(IN user_id INT, IN project_name VARCHAR(255), IN score INT)
+CREATE PROCEDURE AddBonus(IN user_id INT, IN project_name VARCHAR(255), IN score FLOAT)
 BEGIN
     DECLARE project_id INT;
     SELECT id INTO project_id FROM projects WHERE name = project_name;
