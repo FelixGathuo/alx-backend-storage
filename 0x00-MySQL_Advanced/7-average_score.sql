4da9ab0389cf3e96fc724afcbd8eafdb8111ebef
FelixGathuo <40053042+FelixGathuo@users.noreply.github.com>
2023-01-11 21:35:58 +0300

    Update 7-average_score.sql

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,3 +1,5 @@
+-- SQL script that creates a stored procedure ComputeAverageScoreForUser
+-- that computes and store the average score for a student
 CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
 BEGIN
     DECLARE avg_score DECIMAL(5, 2);
