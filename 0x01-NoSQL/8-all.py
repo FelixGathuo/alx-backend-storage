feedb410d52464c0dc4022e12046b5e1b458ea89
FelixGathuo <gathuofelix@gmail.com>
2023-01-16 15:40:25 +0000

    NoSQL

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" a Python function that lists all documents in a collection
+Return an empty list if no document in the collection
+"""
+
+
+def list_all(mongo_collection):
+    documents = []
+    for document in mongo_collection.find():
+        documents.append(document)
+    return documents
---
+++ 0x01-NoSQL/9-insert_school.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""a Python function that inserts a new document in a collection
+based on kwargs and Returns the new _id
+"""
+
+
+def insert_school(mongo_collection, **kwargs):
+    return mongo_collection.insert_one(kwargs).inserted_id
