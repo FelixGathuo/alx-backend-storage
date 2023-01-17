f7e861f68020735cf900d689af221acfce4076a8
FelixGathuo <gathuofelix@gmail.com>
2023-01-17 08:51:13 +0000

    NoSQL

---
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" a Python function that returns the list of school having a specific topic """
+
+
+def schools_by_topic(mongo_collection, topic):
+    """ returns a list of topic """
+    return list(mongo_collection.find({topic}))
