b942f68268abf2a64e0907e44d3cd3d992656a9b
FelixGathuo <gathuofelix@gmail.com>
2023-01-17 11:50:15 +0000

    NoSQL

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -20,8 +20,9 @@ def get_nginx_stats():
         method_count = nginx_collection.count_documents({"method": method})
         print(f"\t{method_count} with method={method}")
 
-    # Count the number of documents with method=GET and path=/status
-    get_status_count = nginx_collection.count_documents({"method": "GET", "path": "/status"})
-    print(f"\t{get_status_count} with method=GET and path=/status")
+        # Count the number of documents with method=GET and path=/status
+        get_status_count = nginx_collection.count_documents({"method": "GET", "path": "/status"})
+        print(f"\t{get_status_count} with method=GET and path=/status")
+
 
 get_nginx_stats()
